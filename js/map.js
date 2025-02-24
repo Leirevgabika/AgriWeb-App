6f5ee31b84a0fb657ff1462fe4496ffff3bdd29f
Leirevgabika <leirevgabika@gmail.com>
2025-02-24 20:37:46 +0100

    Geolocalizacion funcionando

--- js/map.js
+++ js/map.js
@@ -1,12 +1,30 @@
 
-window.onload = function () {
+// funcion que muestra un mapa con leaflet.js en la posicion indicada
+function mostrar_mapa(posicion) {
+    let latitud = posicion.coords.latitude;
+    let longitud = posicion.coords.longitude;
+    let nivel_zoom = 12;
 
+    // mostrar el punto en el mapa con Leaflet.js
+    var map = L.map('map').setView([latitud, longitud], nivel_zoom);
 
-    var map = L.map('map').setView([51.505, -0.09], 13);
-
+    // añadir el mapa base
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
 
+    // añadir control de escala
+    L.control.scale().addTo(map);
+    // añadir un marcador en las coordenadas
+    L.marker([latitud, longitud], { draggable: false }).addTo(map);
+}
+
+window.onload = function () {
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(mostrar_mapa);
+    } else {
+        alert("Este navegador no soporta geolocalizacion");
+    }
 }
